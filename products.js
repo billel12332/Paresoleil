447436264482272df3afc5000ee1a88f7db21a77
billel12332 <132009737+billel12332@users.noreply.github.com>
2023-04-27 21:44:33 +0200

    Add files via upload

---
+++ products.js
@@ -0,0 +1,19 @@
+const products = [
+	{
+		id: 2,
+		logo:'img/logo.png',
+		image: 'img/products/1.png',
+		image2: 'img/products/2.png',
+		image3: 'img/products/3.png',
+		image4: 'img/products/4.png',
+		image5: 'img/products/5.png',
+                image6: 'img/products/6.png',
+		title: '  تمتع ببرودة و حماية عالية لسيارتك مع المظلة العاكسة لأشعة الشمس  الحارقة Pare-soleil pour protégé votre voiture des raillant de soleil ',
+		price1: 4500,
+		price: 3500,
+		description: ' قد يكون التعامل مع حرارة الصيف تجربة مزعجة،خاصة عندما تكون سيارتك تحت أشعة الشمس المباشرة.ولكن مع المظلة العاكسة ،يمكنك التأكد من بقاء سيارتك باردة أثناء ركنها في ظروف القاسية.احصل على عاكسات للأشعة الشمس ولا تقلق أبدًا من ارتفاع درجة حرارة سيارتك مرة أخرى'   ,
+		phone: 0770439221,
+	},
+
+	
+];
